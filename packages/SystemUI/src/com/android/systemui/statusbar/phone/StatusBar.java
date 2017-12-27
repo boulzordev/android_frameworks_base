8811df7a4541b8a18ef9df22836158b01512aa87
Pranav Vashi <neobuddy89@gmail.com>
2017-12-27 09:55:08 +0530

    Recents: Clear locked tasks when using Slim Recents
    * Also improve code base.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -8252,7 +8252,9 @@ public class StatusBar extends SystemUI implements DemoMode,
                     String currentIconPack = (String) newValue;
                     IconPackHelper.getInstance(mContext).updatePrefs(currentIconPack);
                 }
-                mRecents.resetIconCache();
+                if (!mUseSlimRecents) {
+                    mRecents.resetIconCache();
+                }
                 break;
             case NAVIGATION_BAR_VISIBLE:
                 mNavbarVisible =
@@ -8271,7 +8273,8 @@ public class StatusBar extends SystemUI implements DemoMode,
     }
 
     private void updateRecentsMode() {
-        if (mUseSlimRecents) {
+        Recents.mUseSlimRecents = mUseSlimRecents;
+        if (Recents.mUseSlimRecents) {
             mRecents.evictAllCaches();
             mRecents.removeSbCallbacks();
             mSlimRecents = new RecentController(mContext);
@@ -8281,6 +8284,7 @@ public class StatusBar extends SystemUI implements DemoMode,
             }
         } else {
             mRecents.addSbCallbacks();
+            mRecents.resetIconCache();
             if (mSlimRecents != null) {
                 mSlimRecents.evictAllCaches();
                 mSlimRecents.removeSbCallbacks();
